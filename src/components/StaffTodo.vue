901775c526c5454b4550818594260d8fea7dc028
Faith <faithsamuel365@gmail.com>
2019-11-12 01:58:43 +0100

    add  tasks to staff todo

--- src/components/StaffTodo.vue
+++ src/components/StaffTodo.vue
@@ -1,50 +1,79 @@
 <template>
   <div class="container">
-    <form class="box column is-3">
+    <form class="staff box column is-3">
       <p class="subtitle has-text-left">User name</p>
       <hr />
       <h5 class="title is-5">Welcome</h5>
       <b-field>
-        <b-select v-model="time" placeholder="Take a break" rounded>
-          <option v-for="time in timeout" :key="time" value="flint">{{time}}</option>
+        <b-select @input="showTimer($event)" placeholder="Take a break" rounded>
+          <option v-for="time in timeout" :key="time" :value="time">{{time}}</option>
         </b-select>
       </b-field>
-
-      <b-field>
-        <b-input placeholder="Enter a task"></b-input>
-        <p class="control">
-          <button class="button is-info">Add Task</button>
-        </p>
-      </b-field>
-      <div class="content">
-        <ul class="has-text-left">
-          <li>
-            <input type="checkbox" />
-            Task 1
-          </li>
-          <li>
-            <input type="checkbox" />
-            Task 2
-          </li>
-          <li>
-            <input type="checkbox" />
-            Task 3
-          </li>
-        </ul>
+      <div class="timer" v-if="onBreak">
+        <Timer />
+      </div>
+      <div class="Task" v-show="!onBreak">
+        <b-field>
+          <b-input placeholder="Enter a task" v-model="tasks.task"></b-input>
+          <p class="control">
+            <button class="button is-info" @click.prevent="addTask(tasks.task)">Add Task</button>
+          </p>
+        </b-field>
+        <div class="content">
+          <ul class="has-text-left">
+            <li v-for="task in tasks" :key="task">
+              <input type="checkbox" checked :done="tasks.done" v-model="tasks.done" />
+              {{task.task}}
+            </li>
+          </ul>
+        </div>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import Timer from './Timer.vue'
 export default {
+  name: 'StaffTodo',
+  components: {
+    Timer
+  },
   data() {
     return {
       time: '',
-      timeout: ['Take a break', 'Done for today']
+      timeout: ['Take a break', 'Done for today'],
+      onBreak: false,
+      requestType: 'Take a break',
+      tasks: [{ task: 'why are you here', done: false }]
+    }
+  },
+  methods: {
+    showTimer(event) {
+      // console.log(this.timeout[0])
+      // console.log(event)
+
+      if (event === this.timeout[0]) {
+        this.onBreak = true
+      } else {
+        this.onBreak = false
+      }
+    },
+    addTask(task) {
+      this.tasks.push({ task })
+      this.tasks.task = ''
     }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.staff {
+  padding: 1rem 2rem;
+}
+ul {
+  li {
+    list-style-type: none;
+  }
+}
+</style>
